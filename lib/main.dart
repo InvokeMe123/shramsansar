04de4edc42b514f3ca8d3fdbe9ed101a0664c316
InvokeMe <bibashn21@gmail.com>
2024-04-05 11:05:26 +0545

    job application sucessfully

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:logger/logger.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:shramsansar/app/app.dart';
 
+var logger = Logger(
+  printer: PrettyPrinter(),
+);
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 

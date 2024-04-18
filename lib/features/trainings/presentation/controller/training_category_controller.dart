cd55f76f7a65b142b855a48163e0ab728cca68c4
Sulabh Shrestha <shresthasulabh777@gmail.com>
2024-04-18 12:16:24 +0545

    fixed: Training page fetching job category instead of training model

---
+++ lib/features/trainings/presentation/controller/training_category_controller.dart
@@ -0,0 +1,25 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shramsansar/features/jobs/data/models/job_category_model.dart';
+import 'package:shramsansar/features/jobs/data/repository/job_category_repo.dart';
+import 'package:shramsansar/features/trainings/data/models/training_category_model/training_category_model.dart';
+import 'package:shramsansar/features/trainings/data/repository/training_category_repo.dart';
+
+class TrainingCategoryController
+    extends StateNotifier<AsyncValue<TrainingCategoryModel>> {
+  final TrainingCategoryRepo repo;
+  TrainingCategoryController(this.repo) : super(const AsyncValue.loading()) {
+    getJobCategory();
+  }
+  getJobCategory() async {
+    final result = await repo.fetch();
+    result.fold(
+        (l) => state =
+            AsyncValue.error(l.message, StackTrace.fromString(l.message)),
+        (r) => state = AsyncValue.data(r));
+  }
+}
+
+final trainingCategoryControllerProvider = StateNotifierProvider<
+    TrainingCategoryController, AsyncValue<TrainingCategoryModel>>((ref) {
+  return TrainingCategoryController(ref.read(trainingCategoryRepoProvider));
+});
